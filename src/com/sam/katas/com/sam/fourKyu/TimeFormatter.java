1bbeb334e2ce30efaed63517ea34a215c78975d7
Sam Keshmiri <sam.keshmiri@ynap.com>
2019-11-18 14:13:21 +0000

    4 kyu time format completed:

--- src/com/sam/katas/com/sam/fourKyu/TimeFormatter.java
+++ src/com/sam/katas/com/sam/fourKyu/TimeFormatter.java
@@ -1,9 +1,42 @@
 package com.sam.fourKyu;
 
+/**
+ * https://www.codewars.com/kata/52742f58faf5485cae000b9a/solutions/java
+ * Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
+ *
+The function must accept a non-negative integer. If it is zero, it just returns "now". Otherwise, the duration is expressed as a combination of years, days, hours, minutes and seconds.
+
+It is much easier to understand with an example:
+
+TimeFormatter.formatDuration(62)   //returns "1 minute and 2 seconds"
+TimeFormatter.formatDuration(3662) //returns "1 hour, 1 minute and 2 seconds"
+For the purpose of this Kata, a year is 365 days and a day is 24 hours.
+
+Note that spaces are important.
+
+Detailed rules
+The resulting expression is made of components like 4 seconds, 1 year, etc. In general, a positive integer and one of the valid units of time, separated by a space. The unit of time is used in plural if the integer is greater than 1.
+
+The components are separated by a comma and a space (", "). Except the last component, which is separated by " and ", just like it would be written in English.
+
+A more significant units of time will occur before than a least significant one. Therefore, 1 second and 1 year is not correct, but 1 year and 1 second is.
+
+Different components have different unit of times. So there is not repeated units like in 5 seconds and 1 second.
+
+A component will not appear at all if its value happens to be zero. Hence, 1 minute and 0 seconds is not valid, but it should be just 1 minute.
+
+A unit of time must be used "as much as possible". It means that the function should not return 61 seconds, but 1 minute and 1 second instead. Formally, the duration specified by of a component must not be greater than any valid more significant unit of time.
+ * 
+ * 
+ * 
+ * @author s.keshmiri
+ *
+ */
+
 public class TimeFormatter {
 	
 	public static void main(String[] args) {
-		System.out.println(TimeFormatter.formatDuration(132030240));
+		System.out.println(TimeFormatter.formatDuration(205851834));
 	}
 	
     public static String formatDuration(int seconds) {
@@ -46,7 +79,13 @@ public class TimeFormatter {
     	if (days == 0 && hours == 0 && minutes == 0 && secs == 0) {
     		return sb.toString();
     	}
-    	if (years > 0 && days > 0 ^ hours > 0 ^ minutes > 0 ^ secs > 0 && !sb.toString().contains("and") && (days > 0 && hours > 0 && minutes > 0 && secs > 0)) {
+    	
+    	if ((hours > 0  ^ minutes > 0 ^ secs > 0) && (days > 0 && hours > 0 && minutes > 0 && secs > 0) && years > 0
+    			&& ((days == 0 && hours == 0 && minutes == 0 && secs > 0)
+    				|| days == 0 && hours == 0 && minutes > 0 && secs == 0
+    				|| days == 0 && hours > 0 && minutes == 0 && secs == 0
+    				|| days > 0 && hours == 0 && minutes == 0 && secs == 0)			
+    			) {
     		sb.append(" and ");
     	}
 
@@ -57,7 +96,7 @@ public class TimeFormatter {
     		return sb.toString();
     	}
     
-    	if (!(hours > 0 ^ minutes > 0 ^ secs > 0) && !sb.toString().contains("and") && (hours > 0 && minutes > 0 && secs > 0)) {
+    	if (hours > 0 ^ minutes > 0 ^ secs > 0 && !(hours > 0 && minutes > 0 && secs > 0) && (years > 0 || days > 0)) {
     		sb.append(" and ");
     	}
     	
@@ -69,7 +108,8 @@ public class TimeFormatter {
     	}
     	
     	sb.append(hours >= 1 && !(minutes > 0 ^ secs > 0) ? ", " : "");
-    	if (!(minutes > 0 ^ secs > 0) && !sb.toString().contains("and") && secs > 0 && !(minutes > 0 && secs > 0)) {
+    	
+    	if ((minutes > 0 || secs > 0) && ((minutes > 0 && !(secs >0)) || (secs > 0 && !(minutes > 0))) && (hours > 0 || days > 0 || years > 0)) {
     		sb.append(" and ");
     	}
     	
@@ -79,7 +119,7 @@ public class TimeFormatter {
     		return sb.toString();
     	}
     	
-    	if (secs > 0 && !sb.toString().contains("and")) {
+    	if (!(minutes > 0 ^ secs > 0) && secs > 0) {
     		sb.append(" and ");
     	}
     	
