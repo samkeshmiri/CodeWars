c560d074ed48db2d205933e0c89562b27d4f9d25
Sam Keshmiri <samkeshmiri94@gmail.com>
2020-03-05 11:47:26 +0000

    working tribonnaci kata

---
+++ src/com/sam/katas/com/sam/sixKyu/Xbonacci.java
@@ -0,0 +1,33 @@
+package com.sam.sixKyu;
+
+// https://www.codewars.com/kata/556deca17c58da83c00002db/java
+
+public class Xbonacci {
+	
+	public static void main(String[] args) {
+		System.out.println(tribonacci(new double[] {1,1,1,}, 1));
+	}
+	
+	public static double[] tribonacci(double[] s, int n) {
+		double[] arr = new double[n];
+		
+		if (n == 0) {
+			return new double[0];
+		}
+		
+		if (n < 3) {
+			return new double[] {s[0]};
+		}
+		
+		for (int k = 0; k < s.length; k++) {
+			arr[k] = s[k];
+		}
+		
+		for (int i = 0; i < n - 3; i++) {
+			arr[3 + i] = arr[i] + arr[i+1] + arr[i+2];
+		}
+				
+		return arr;
+	}
+
+}
