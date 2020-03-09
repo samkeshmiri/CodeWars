c7208722ae20d28e51bb4bee5d885d3f67773ebb
Sam Keshmiri <samkeshmiri94@gmail.com>
2020-03-09 10:00:30 +0000

    refactored fib prod

--- src/com/sam/katas/com/sam/fiveKyu/ProdFib.java
+++ src/com/sam/katas/com/sam/fiveKyu/ProdFib.java
@@ -10,17 +10,14 @@ public class ProdFib {
 
 	public static void main(String[] args) {
 		
-		System.out.println(productFib(714));	// should return {21, 34, 1}, 
-		System.out.println(productFib(4895));	// [2178309, 3524578, 0]
+		System.out.println(Arrays.toString(productFib(714)));	// should return {21, 34, 1}, 
+		System.out.println(Arrays.toString(productFib(4895)));	// [2178309, 3524578, 0]
 	}
 
 	public static long[] productFib(long prod) {
 		List<Long> list = new ArrayList<>();
 		long[] array = new long[3];
-		Long current = 0L;
-		Long a = 0L;
-		Long b = 0L;
-		Long c = 0L;
+		Long current = 0L, a = 0L, b = 0L, c  = 0L; // probably not best practice but just testing
 		
 		list.add(0L);
 		list.add(1L);
@@ -32,19 +29,16 @@ public class ProdFib {
 		for (int i = 0; current <= prod; i++) {
 			list.add(list.get(i) + list.get(i + 1));
 			current = list.get(list.size() - 1);
-			System.out.println(current);
 		}
 
 		for (int k = 0; k < list.size() - 1; k++) {
+			a = list.get(k);
+			b = list.get(k + 1);
 			if (list.get(k) * list.get(k + 1) == prod) {
-				a = list.get(k);
-				b = list.get(k + 1);
 				c = 1L;
 				break;
 			}
 			if (list.get(k) * list.get(k+1) > prod) {
-				a = list.get(k);
-				b = list.get(k+1);
 				c = 0L;
 				break;
 			}
