511d53e2aba93864c37b41f24d6a80f2c4f1f4d9
s.keshmiri <sam.keshmiri@ynap.com>
2021-06-28 14:24:24 +0100

    StringAnalyzer working using Set

---
+++ src/com/sam/katas/com/sam/fiveKyu/StringAnalyzer.java
@@ -0,0 +1,33 @@
+package com.sam.fiveKyu;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * You have a string and you want to find all the possible unique substrings this string has.
+ * Specifically, you should be returning the total number of unique substrings.
+ * https://www.codewars.com/kata/55a304533d16fd5b0200006b/train/java
+ */
+public class StringAnalyzer {
+    
+	public static void main(String[] args) {
+		System.out.println(getSubstrings("YOLO")); // 9
+	}
+	
+    private static int getSubstrings (String source) {
+    	Set<String> set = new HashSet<>();
+    	
+    	for(int i = 0; i < source.length(); i++) {
+    		String word = source.substring(0, source.length()-i);
+    		set.add(word);
+    		for (int k = 1; k < word.length(); k++) {
+    			String subWord = word.substring(k);
+    			set.add(subWord);
+    		}
+    	}
+    
+    	return set.size();
+    }
+	
+
+}
