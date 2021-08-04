6acbd0a45a822028cf303fb68974606c71730471
s.keshmiri <sam.keshmiri@ynap.com>
2021-08-04 16:43:20 +0100

    camel case builder complete

---
+++ src/com/sam/katas/com/sam/sixKyu/CamelCaseBuilder.java
@@ -0,0 +1,35 @@
+package com.sam.sixKyu;
+
+/**
+ * https://www.codewars.com/kata/517abf86da9663f1d2000003 Complete the
+ * method/function so that it converts dash/underscore delimited words into
+ * camel casing. The first word within the output should be capitalized only if
+ * the original word was capitalized (known as Upper Camel Case, also often
+ * referred to as Pascal case).
+ * 
+ * @author s.keshmiri
+ *
+ */
+public class CamelCaseBuilder {
+
+	public static void main(String[] args) {
+		System.out.println(toCamelCase("the_stealth-warrior"));
+	}
+
+	static String toCamelCase(String s) {
+		String[] arr = s.split("[-_]");
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < arr.length; i++) {
+			if (i == 0) {
+				sb.append(arr[0]);
+				continue;
+			}
+			String camelWord = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+			sb.append(camelWord);
+		}
+
+		return sb.toString();
+	}
+
+}
