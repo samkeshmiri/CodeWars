d6783f53c174c007d3f35266b19375a206af379c
s.keshmiri <sam.keshmiri@ynap.com>
2021-08-05 17:52:45 +0100

    used stream to count both sides of array

---
+++ src/com/sam/katas/EqualArraySides.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+/**
+ * https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/java
+ * You are going to be given an array of integers. 
+ * Your job is to take that array and find an index N,
+ * where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. 
+ * If there is no index that would make this happen, return -1.
+ * 
+ * e.g. 1,2,3,4,3,2,1 would return 3 (index at which left side of it is equal to right side of it).
+ * 
+ * @author s.keshmiri
+ *
+ */
+public class EqualArraySides {
+
+	public static int findEvenIndex(int[] arr) {
+		
+		if (arr.length == 0) {
+			return 0;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			int left = Arrays.stream(arr, 0, i).sum();
+			int right = Arrays.stream(arr, i + 1, arr.length).sum();
+
+			if (left == right) {
+				return i;
+			}
+
+		}
+		
+		return -1;
+	}
+}
---
+++ src/com/sam/katas/EqualArraySidesTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EqualArraySidesTest {
+
+	@Test
+	public void test() {
+		assertEquals(3, EqualArraySides.findEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+		assertEquals(1, EqualArraySides.findEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+		assertEquals(-1, EqualArraySides.findEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+		assertEquals(3, EqualArraySides.findEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
+		assertEquals(-1, EqualArraySides.findEvenIndex(new int[] { -8505, -5130, 1926, -9026 }));
+		assertEquals(1, EqualArraySides.findEvenIndex(new int[] { 2824, 1774, -1490, -9084, -9696, 23094 }));
+		assertEquals(6, EqualArraySides.findEvenIndex(new int[] { 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4 }));
+
+//		assertEquals(0, EqualArraySides.findEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+//		assertEquals(0, EqualArraySides.findEvenIndex(new int[] { -8505, -5130, 1926, -9026 }));
+//		assertEquals(0, EqualArraySides.findEvenIndex(new int[] { 8, 8 }));
+		assertEquals(0, EqualArraySides.findEvenIndex(new int[] { 8, 0 }));
+	}
+
+}
