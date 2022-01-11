91533988684b9874c97ce8d089aff99cc2bccd00
Sam Keshmiri <skeshmiri@Sams-MacBook-Pro.local>
2022-01-11 14:42:27 +0000

    optimized algorithm

--- typescript/src/6kyu/RomanNumeralsToDigits.ts
+++ typescript/src/6kyu/RomanNumeralsToDigits.ts
@@ -1,37 +1,26 @@
 const map = new Map<string, number>();
 map.set("I", 1);
-map.set("IV", 4);
 map.set("V", 5);
-map.set("IX", 9);
 map.set("X", 10);
-map.set("XL", 40);
 map.set("L", 50);
-map.set("XC", 90);
 map.set("C", 100);
-map.set("CD", 400);
 map.set("D", 500);
-map.set("CM", 900);
 map.set("M", 1000);
 
 function fromRoman(romanNumber: string) {
-    if (map.has(romanNumber)) {
-        return map.get(romanNumber)!;
-    } else {
-        let result = 0;
+    let result = 0;
 
-        for (let i = 0; i < romanNumber.length - 1; i++) {
-            let numeral = romanNumber.substring(i, i + 2);
-            if (map.has(numeral)) {
-                result += map.get(numeral)!;
-                romanNumber = romanNumber.replace(numeral, "");
-            }
-        }
+    for (let i = 0; i < romanNumber.length; i++) {
+        let current = map.get(romanNumber[i])!;
+        let next = map.get(romanNumber[i + 1]) || 0;
 
-        //solve last 2 letters
-        for (let i = 0; i < romanNumber.length; i++) {
-            result += map.get(romanNumber[i])!;
+        if (current < next) {
+            result -= current;
+        } else {
+            result += current;
         }
-        return result;
     }
+    return result
 }
-console.log(fromRoman("MMXXII"));
+
+console.log(fromRoman("XXI"));
